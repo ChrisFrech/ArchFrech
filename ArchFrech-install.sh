ddeab90531ab2c2fcac63de19b3f271c1c2e965b
ChrisFrech <76786592+ChrisFrech@users.noreply.github.com>
2022-10-01 18:28:45 +0200

    Amended commentary

--- ArchFrech-install.sh
+++ ArchFrech-install.sh
@@ -1,13 +1,13 @@
 $ sudo pacman -S $(awk '{print $1}'  packages.txt)
 
-// Clone "yay" AUR package helper
+# Clone "yay" AUR package helper
 && git clone https://aur.archlinux.org/yay.git
 
-// Go to install directory
+# Go to install directory
 && cd yay
 
-// Install "yay"
+# Install "yay"
 && makepkg -si --noconfirm
 
-//  Go back to main directory
+#  Go back to main directory
 && cd ..
